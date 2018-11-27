8b8fb8adf05c73d5fbf65403bc00d154f5480a7f
Aroldo Felix <junioraroldo37@gmail.com>
2018-11-27 07:42:25 -0300

    Vai dar certo

---
+++ ProjetoInvestigacao/src/main/java/ArvoreInvestigadora/ProjetoInvestigacao/Arvore.java
@@ -0,0 +1,8 @@
+package ArvoreInvestigadora.ProjetoInvestigacao;
+import java.util.LinkedList;
+
+public class Arvore {
+	// Criando o nó da árvore
+	LinkedList<User> usuarios;
+	
+}
---
+++ ProjetoInvestigacao/src/main/java/ArvoreInvestigadora/ProjetoInvestigacao/Type.java
@@ -0,0 +1,5 @@
+package ArvoreInvestigadora.ProjetoInvestigacao;
+
+public class Type {
+
+}
